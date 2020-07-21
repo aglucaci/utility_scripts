9525453cc1179954b45c343c1656b7364770cd62
Alexander Lucaci <43097993+aglucaci@users.noreply.github.com>
2020-07-21 15:37:25 -0400

    Add files via upload
    initial commit, can be modified to run any batch file

---
+++ RUN_BUSTEDS_BUSTEDSMH.sh
@@ -0,0 +1,88 @@
+#!/bin/bash
+
+clear
+
+echo ""
+echo "# ###"
+echo ""
+
+
+BASEDIR="/Users/user/Documents/EmpiricalDatasets"
+
+# Available online at
+# https://github.com/veg/hyphy/blob/master/res/TemplateBatchFiles/SelectionAnalyses/BUSTED.bf
+# https://github.com/veg/hyphy-analyses/blob/master/BUSTED-MH/BUSTED-MH.bf
+
+BUSTEDS=$BASEDIR"/hyphy-develop/res/TemplateBatchFiles/SelectionAnalyses/BUSTED.bf"
+BUSTEDSMH=$BASEDIR"/hyphy-analyses/BUSTED-MH/BUSTED-MH.bf"
+HYPHY=$BASEDIR"/hyphy-develop/hyphy"
+res=$BASEDIR"/hyphy-develop/res"
+
+echo "# Settings"
+echo "BASEDIR: "$BASEDIR
+echo "HYPHY: "$HYPHY
+echo "Resources: "$res
+echo "BUSTEDS batch file: "$BUSTEDS
+echo "BUSTEDS-MH batch file: "$BUSTEDSMH
+echo ""
+
+echo "# File checks"
+
+# File checks
+if [ -f "$HYPHY" ]; then
+    echo "$HYPHY exists."
+else
+    echo "$HYPHY does not exist."
+fi
+
+if [ -f "$BUSTEDS" ]; then
+    echo "$BUSTEDS exists."
+else
+    echo "$BUSTEDS does not exist."
+fi
+
+if [ -f "$BUSTEDSMH" ]; then
+    echo "$BUSTEDSMH exists."
+else
+    echo "$BUSTEDSMH does not exist."
+fi
+# End file checks
+echo ""
+
+
+echo "Starting to process files..."
+
+for file in $BASEDIR/11-datasets/*.nex; do
+    echo "Processing: "$file
+    
+    #Does the nexus file exist and is not empty?
+    if [ -s "$file" ]; then
+        echo "    $file exists and is not empty"
+        
+        # [1] Process it for BUSTEDS
+        CHECK=$file".BUSTEDS.json"
+        if [ -s "$CHECK" ]
+        then
+           echo "    BUSTEDS file exists and is not empty "
+        else
+           echo "    BUSTEDS file does not exist, or is empty "
+           $HYPHY $BUSTEDS LIBPATH=$res --alignment $file --output $file".BUSTEDS.json"
+        fi
+    
+        CHECK=$file".BUSTEDS-MH.json"
+        # [2] Process it for BUSTEDS-MH
+        if [ -s "$CHECK" ]
+        then
+           echo "    BUSTEDS-MH file exists and is not empty "
+        else
+           echo "    BUSTEDS-MH file does not exist, or is empty "
+           $HYPHY $BUSTEDSMH LIBPATH=$res --alignment $file --output $file".BUSTEDS-MH.json"
+        fi
+    else
+        echo "    $file does not exist or is empty"
+    fi
+    
+    #$HYPHY $BUSTEDS --alignment $file --output $file".BUSTEDS.json"
+    
+    #$HYPHY $BUSTEDSMH --alignment $file --output $file".BUSTEDS-MH.json"
+done
