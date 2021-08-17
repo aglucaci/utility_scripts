ec191af73a19570234b67362a58b6b3c8a39ce20
Alexander Lucaci <43097993+aglucaci@users.noreply.github.com>
2021-08-17 15:52:59 -0400

    Update install_hyphy.sh

--- install_hyphy.sh
+++ install_hyphy.sh
@@ -6,22 +6,30 @@ git clone --branch develop https://github.com/veg/hyphy.git hyphy-develop
 
 cd hyphy-develop
 
-cmake ./
+# locally
+#cmake ./
+
 # for silverback
 #cmake . -DNOAVX=ON
+cmake -DCMAKE_INSTALL_PREFIX:PATH=/home/aglucaci/hyphy-develop -DNOAVX=ON .
 
+#local
 make -j MP
-# for silverback
-#make -j MPI
 
+# for silverback
+make -j MPI
+
+# Finally
 make install
+
+#Tests
+make test
 
 #Download hyphy standalone analyses
 cd ..
 git clone https://github.com/veg/hyphy-analyses.git
 
 #End of file
-
 
 
 
